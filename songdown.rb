37105eb45e8ec57b3f5e1f16403b068c4257cc2f
Vasari <nmccallum7@gmail.com>
2014-08-06 17:26:46 +1000

    Basic implementation of the stuffz.

--- songdown.rb
+++ songdown.rb
@@ -20,14 +20,39 @@ def compile_songdown(text)
 
     lines = text.split "\n"
 
+    # Some variables for tracking where we are...
+    in_verse = false
+    verse_num = 0
+
     lines.each do |line|
         # Here comes the real fun...
 
         # Verse headers.
         if line =~ /\S+:$/
-            output << "<i>#{line}</i><br />"
-            next
+            line = "<span class='verse-head'>#{line}</span>"
+            in_verse = true
         end
+
+        # Verse end marks
+        if line =~ /^---/
+            line = '' # Don't show the end mark
+            in_verse = false
+            verse_num = 0
+        end
+
+        # Bold all the chords, assuming every second line in a verse is comprised of chords.
+        if verse_num.odd? && !verse_num.zero? && in_verse == true
+            line = line.gsub /\s/, '&nbsp;'
+            line = "<span class='chords'>#{line}</span>"
+        elsif verse_num.even? && !verse_num.zero? && in_verse == true
+            line = "<span class='lyrics'>#{line}</span>"
+        end
+
+
+        line += "<br />\n"
+        output << line
+
+        verse_num += 1 if in_verse
     end
 
     output.join ''
@@ -39,5 +64,7 @@ files.each do |name|
     opath = File.join(OUTPUT, name).gsub! /songdown$/, 'html'
     text = File.read path
     html = compile_songdown text
+    html = TEMPLATE.call :song_html => proc { Handlebars::SafeString.new html }
     File.write(opath, html)
+    # puts html
 end
