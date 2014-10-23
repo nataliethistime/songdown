0dc51f3f22078dbcb7c1a891ea1c9ba980394b00
Vasari <nmccallum7@gmail.com>
2014-10-23 13:01:03 +1000

    Improvements to deploy script output.

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,12 @@
+@echo off
+
+
+echo ---------- Pushing to Github ----------
+echo.
+echo.
 git push origin master
+
+echo ---------- Pushing to Heroku ----------
+echo.
+echo.
 git push heroku master
